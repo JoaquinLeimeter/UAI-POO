905ef5d051c5fec6359ef419ab97b2f2ecb1d991
JoaquinLeimeter <joaquin.leimeter@radiumrocket.com>
2022-04-26 15:18:57 -0300

    clase 4 con WF

---
+++ 4-ejercicio-WF/ejercicio1/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace ejercicio1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvEmpleados = new System.Windows.Forms.DataGridView();
+            this.nombreLabel = new System.Windows.Forms.Label();
+            this.apellidoLabel = new System.Windows.Forms.Label();
+            this.edadLabel = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.apellido = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.edad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.index = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmpleados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvEmpleados
+            // 
+            this.dgvEmpleados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEmpleados.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nombre,
+            this.apellido,
+            this.edad});
+            this.dgvEmpleados.Location = new System.Drawing.Point(371, 21);
+            this.dgvEmpleados.Name = "dgvEmpleados";
+            this.dgvEmpleados.Size = new System.Drawing.Size(400, 229);
+            this.dgvEmpleados.TabIndex = 0;
+            this.dgvEmpleados.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvEmpleados_CellClick);
+            // 
+            // nombreLabel
+            // 
+            this.nombreLabel.AutoSize = true;
+            this.nombreLabel.Location = new System.Drawing.Point(69, 44);
+            this.nombreLabel.Name = "nombreLabel";
+            this.nombreLabel.Size = new System.Drawing.Size(44, 13);
+            this.nombreLabel.TabIndex = 1;
+            this.nombreLabel.Text = "Nombre";
+            // 
+            // apellidoLabel
+            // 
+            this.apellidoLabel.AutoSize = true;
+            this.apellidoLabel.Location = new System.Drawing.Point(69, 80);
+            this.apellidoLabel.Name = "apellidoLabel";
+            this.apellidoLabel.Size = new System.Drawing.Size(44, 13);
+            this.apellidoLabel.TabIndex = 2;
+            this.apellidoLabel.Text = "Apellido";
+            // 
+            // edadLabel
+            // 
+            this.edadLabel.AutoSize = true;
+            this.edadLabel.Location = new System.Drawing.Point(69, 121);
+            this.edadLabel.Name = "edadLabel";
+            this.edadLabel.Size = new System.Drawing.Size(32, 13);
+            this.edadLabel.TabIndex = 3;
+            this.edadLabel.Text = "Edad";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(124, 41);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 4;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(124, 77);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(100, 20);
+            this.txtApellido.TabIndex = 5;
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Location = new System.Drawing.Point(124, 118);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(100, 20);
+            this.txtEdad.TabIndex = 6;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(33, 159);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(304, 23);
+            this.addButton.TabIndex = 7;
+            this.addButton.Text = "Agregar";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // nombre
+            // 
+            this.nombre.HeaderText = "Nombre";
+            this.nombre.Name = "nombre";
+            // 
+            // apellido
+            // 
+            this.apellido.HeaderText = "Apellido";
+            this.apellido.Name = "apellido";
+            // 
+            // edad
+            // 
+            this.edad.HeaderText = "Edad";
+            this.edad.Name = "edad";
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(33, 188);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(304, 23);
+            this.deleteButton.TabIndex = 8;
+            this.deleteButton.Text = "Borrar";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // index
+            // 
+            this.index.AutoSize = true;
+            this.index.Location = new System.Drawing.Point(368, 253);
+            this.index.Name = "index";
+            this.index.Size = new System.Drawing.Size(13, 13);
+            this.index.TabIndex = 9;
+            this.index.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.index);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.edadLabel);
+            this.Controls.Add(this.apellidoLabel);
+            this.Controls.Add(this.nombreLabel);
+            this.Controls.Add(this.dgvEmpleados);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmpleados)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvEmpleados;
+        private System.Windows.Forms.Label nombreLabel;
+        private System.Windows.Forms.Label apellidoLabel;
+        private System.Windows.Forms.Label edadLabel;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn apellido;
+        private System.Windows.Forms.DataGridViewTextBoxColumn edad;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Label index;
+    }
+}
+
---
+++ 4-ejercicio-WF/ejercicio1/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejercicio1
+{
+    public partial class Form1 : Form
+    {
+        private int rowIndex = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+
+            //validar que haya algo en los inputs
+            if(txtNombre.Text + txtApellido.Text + txtEdad.Text != "")
+            {
+                //agregamos columna, nos devuelve el indice de esa columna.
+                int n = dgvEmpleados.Rows.Add();
+                //usando el indice agregamos informacion
+                dgvEmpleados.Rows[n].Cells[0].Value = txtNombre.Text;
+                dgvEmpleados.Rows[n].Cells[1].Value = txtApellido.Text;
+                dgvEmpleados.Rows[n].Cells[2].Value = txtEdad.Text;
+            }
+            
+
+            //clear inputs
+            txtNombre.Text = "";
+            txtApellido.Text = "";
+            txtEdad.Text = "";
+        }
+
+        private void dgvEmpleados_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            rowIndex = e.RowIndex;
+            index.Text = (rowIndex + 1).ToString();
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            dgvEmpleados.Rows.RemoveAt(rowIndex);
+        }
+    }
+}
