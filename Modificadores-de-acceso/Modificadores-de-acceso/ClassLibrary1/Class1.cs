c4b44927f88456c732159f056b8132d760a79cfd
Joaquin Leimeter <joaquin.leimeter@radiumrocket.com>
2022-05-31 15:43:40 -0300

    modificadores de acceso

---
+++ Modificadores-de-acceso/Modificadores-de-acceso/ClassLibrary1/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        private Class1() { }
+        public void someMethod()
+        {
+            Class2 obj = new Class2();
+            obj.DoStuff();
+        }
+        //Que pasa?
+        Class2.ClassInsideClass2 obj2 = new Class2.ClassInsideClass2();
+    }
+
+    //cambiar a public para que funcione ClassLibrary2
+    internal class Class2
+    {
+        //en cualquiera de los 3 casos puedo hacer esto,
+        //a no ser que haga privado el 
+        public Class2() { }
+        //este método es publico pero inaccesible en ClassLibrary2
+        public void DoStuff()
+        {
+            Console.WriteLine("Doing stuff");
+        }
+        //Que pasa en Class2 si esta clase es internal, private?
+        public class ClassInsideClass2
+        {
+            //Que pasa en Class2 si metodo es internal, private?
+            public ClassInsideClass2() { }
+        }
+    }
+}
---
+++ Modificadores-de-acceso/Modificadores-de-acceso/ClassLibrary2/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace ClassLibrary2
+{
+    public class Class1
+    {
+        Class2 instans = new Class2();
+        ClassLibrary1.Class2 instancia = new ClassLibrary1.Class2();
+
+        public void OtherMethod()
+        {
+            instancia.DoStuff();
+        }
+    } 
+}
