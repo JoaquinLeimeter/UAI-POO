ea23395c534ee0521814b3e7a57b40091f431d3f
Joaquin Leimeter <joaquin.leimeter@radiumrocket.com>
2022-05-31 15:43:40 -0300

    modificadores de acceso

---
+++ Modificadores-de-acceso/Modificadores-de-acceso/ClassLibrary2/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace ClassLibrary2
+{
+    public class Class1
+    {
+        ClassLibrary1.Class1 instance = new ClassLibrary1.Class1();
+    } 
+}
